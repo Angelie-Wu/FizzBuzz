31ff4932d71c18352943592e201d0212987ec846
Angelie-Wu <angeliewu514@gmail.com>
2025-09-08 20:31:56 -0400

    change helping method to public

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -8,7 +8,7 @@ public class Multiples {
         System.out.println(count);
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int counter = 0;
         for (int i = 1; i < n; i++){
             if(i % a == 0 || i % b == 0){
@@ -18,7 +18,7 @@ public class Multiples {
         return counter;
     }
 
-    static int multiples(){
+    public static int multiples(){
         return multiples(1000,3,5);
     }
 }
